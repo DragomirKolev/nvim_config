5b9120e9b9f720c5dc625bb74587e50b7e05e105
Dragomir Kolev <dragomir.kolev@egt-digital.com>
2024-06-17 08:31:16 +0300

    Initial commit

---
+++ init.lua
@@ -0,0 +1,6 @@
+require("drkolev");
+require("drkolev.lazy");
+
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 0
+vim.g.netrw_sizestyle = "H"
---
+++ lua/drkolev/plugins/init.lua
@@ -0,0 +1,8 @@
+return {
+  {
+      "nvim-lua/plenary.nvim",
+      name = "plenary"
+  },
+  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  "nvim-tree/nvim-web-devicons"
+}
