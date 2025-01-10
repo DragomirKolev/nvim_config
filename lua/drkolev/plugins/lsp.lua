c6df5651964cd34e74630aa6d82e4899df0ffdd7
DragomirKolev <dragomir.kolev@egt-digital.com>
2025-01-10 11:55:36 +0200

    task(typescript-svelte): update to buffer not updating on type change

--- lua/drkolev/plugins/lsp.lua
+++ lua/drkolev/plugins/lsp.lua
@@ -83,7 +83,7 @@ return {
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<M-e>"] = cmp.mapping.complete(),
+                ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
                 { name = 'nvim_lsp' },
